a6ca79f8bdd80cd3da31f906267fdf05747ddd27
Thibault NOGHERO <thibault.noghero@gmail.com>
2023-01-21 18:35:43 +0100

    Header burger menu work (js + css) 21/01/21/2023 18:35

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
-const switch_button = document.getElementById()
+const switch_button = document.getElementById("bouton_burger");
+const menu = document.getElementById("menu_invisible");
+
+function menu_switch(){
+    if (menu.style.display === "none"){
+        menu.style.display = "block";
+    }else{
+        menu.style.display = "none"
+    }
+}
+
+switch_button.addEventListener("click", menu_switch);
+
+
+
+
+

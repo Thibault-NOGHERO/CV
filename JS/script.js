5aad9af6ad15230b7a561061ac9b3be5e862ca02
Thibault NOGHERO <thibault.noghero@gmail.com>
2023-01-21 20:13:36 +0100

    Fin première journée

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,10 @@
 const switch_button = document.getElementById("bouton_burger");
 const menu = document.getElementById("menu_invisible");
 
-
-function aanimation(){
-    if (menu.classList.contains("open")){
-        menu.classList.remove("open");
-        menu.style.display = "none";
-    }else{
-        menu.classList.add("open");
-        menu.style.display = "block";
-    }
-}
-// switch_button.addEventListener("click", aanimation);
-
-
-// ZONE DE TEST
-
+// APPARITION MENU PLUS
 function apparition(){
     menu.classList.toggle('show');
 }
 switch_button.addEventListener("click", apparition);
+
+// ZONE DE TEST
